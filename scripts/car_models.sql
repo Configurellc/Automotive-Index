ed06386d34a4f78af9cfceb8770c9bb716285311
Joshua Lee <joshualee@iconfigure.org>
2017-01-21 17:44:59 -1000

    recorded index statements

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,8 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+CREATE INDEX automotive_index
+ON "car_models" ("make_code", "year");
+
